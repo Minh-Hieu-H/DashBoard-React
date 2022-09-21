8775b1bbd1b0f453484e1f242d38a581e57d0154
Lap trinh vien A <ninhthaithan20@gmail.com>
2022-09-21 22:26:15 +0700

    Create project

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+import { Provider } from 'react-redux'
+
+
+
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import './assets/css/grid.css'
+import './assets/css/theme.css'
+import './assets/css/index.css'
+
+import Layout from './components/layout/Layout'
+
+document.title = 'Tua CRM'
+
+ReactDOM.render(
+  <>
+    <React.StrictMode>
+      <Layout />
+    </React.StrictMode>
+  </>,
+  document.getElementById('root')
+);
+
